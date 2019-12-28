a54cc5baff66f7d01207abb727d865548da2c0c1
Wincy LAW <wincylaw@Wincys-MacBook-Pro.local>
2019-12-28 15:54:00 -0800

    updated CORS with dynamic origin

--- server.js
+++ server.js
@@ -26,9 +26,22 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(cors({origin: 'https://gaoutcomes-frontend.herokuapp.com'}));
-
+// app.use(cors({origin: 'https://gaoutcomes-frontend.herokuapp.com'}));
 // app.use(cors())
+
+const whitelist = ['http://localhost:3000', 'https://gaoutcomes-frontend.herokuapp.com'];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+app.use(cors(corsOptions));
 
 app.use("/students", studentsController);
 app.use("/courses", coursesController);
